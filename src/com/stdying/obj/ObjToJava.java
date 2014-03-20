0cc0dbc5db1a75c3d9b8355cde8587f73784c372
stdying <stdyingbook@gmail.com>
2014-03-20 09:06:14 +0800

    JNI 静态变量使用

---
+++ src/com/stdying/obj/ObjToJava.java
@@ -0,0 +1,8 @@
+package com.stdying.obj;
+
+public class ObjToJava {
+	public native Persion getPersionInit();
+	public native Persion getPersion();
+	
+	
+}
---
+++ src/com/stdying/obj/Persion.java
@@ -0,0 +1,15 @@
+package com.stdying.obj;
+
+public class Persion {
+	private int age;
+	private String name;
+	
+	public Persion(){
+		
+	}
+	
+public Persion(int age,String name){
+		this.age = age;
+		this.name = name;
+	}
+}
